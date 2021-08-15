3b73da85c58da3b7c97e9aed78ae23b312c5fa92
@NodirMN <998946140194@mail.ru>
2021-08-14 16:18:22 -1200

    2dars

---
+++ JS-2.js
@@ -0,0 +1,25 @@
+////Hafta kunlari
+
+let day = 2
+if (day==1){
+    console.log('Dushanba');
+}else if (day==2){
+    console.log('Seshanba');
+}
+else if (day==3){
+    console.log('Chorshanba');
+}
+else if (day==4){
+    console.log('Payshanba');
+}
+else if (day==5){
+    console.log('Juma');
+}
+else if (day==6){
+    console.log('Shanba');
+}
+else if (day==7){
+    console.log('Yakshanba');
+}else{
+console.log('Bunday hafta yo`q');
+}
