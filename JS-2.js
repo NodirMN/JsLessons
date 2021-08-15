22352980f4fdcb62efa0c8b79cd3229d680cf9e8
@NodirMN <998946140194@mail.ru>
2021-08-14 18:06:52 -1200

    Update JS-2.js

--- JS-2.js
+++ JS-2.js
@@ -1,9 +1,9 @@
 ////Hafta kunlari
 
-let day = 2
+let day = 0
 if (day==1){
     console.log('Dushanba');
-}else if (day==2){
+}else if (day == 2){
     console.log('Seshanba');
 }
 else if (day==3){
@@ -12,13 +12,13 @@ else if (day==3){
 else if (day==4){
     console.log('Payshanba');
 }
-else if (day==5){
+else if (day == 5){
     console.log('Juma');
 }
-else if (day==6){
+else if (day == 6){
     console.log('Shanba');
 }
-else if (day==7){
+else if (day == 7){
     console.log('Yakshanba');
 }else{
 console.log('Bunday hafta yo`q');
@@ -29,4 +29,32 @@ if (day<=7 && day!==0){
     console.log(weekDays[day-1]);// massivdan -1 qilib olib tashash kere
 }else {
     console.log('Bunday hafta yo`q');
+}
+
+////Baxolar
+let b = 2
+
+if (b == 1){
+    console.log('Yomon');
+}else if(b == 2){
+    console.log('Qoniqarsiz');
+}
+else if(b == 3){
+    console.log('Qoniqarli');
+}
+else if(b == 4){
+    console.log('Yaxshi');
+}
+else if(b == 5){
+    console.log('A`lo');
+}else{
+    console.log('1dan 5gacham bahoni tanlang');
+}
+
+
+let baholar = ['Yomon','Qoniqarsiz','Qoniqarli','Yahshi','A`lo']
+if(b<=5 && b!==0){
+    console.log(baholar[b-1])
+}else{
+    console.log('Xatolik');
 }
