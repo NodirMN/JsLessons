bc0fb77f9c7fe3218e67458f1f9e76f0d67b183e
@NodirMN <998946140194@mail.ru>
2021-08-14 20:04:05 -1200

    Update JS-3.js

---
+++ JS-3.js
@@ -0,0 +1,15 @@
+////Sikil for
+let summa = 0
+for(let a = 0; a<=10; a++){
+    console.log('summa=' , summa, 'a=' , a);
+        summa+=a
+}
+console.log(summa);
+
+///// Juft son
+let juft = 1
+for(let b = 2; b<=10; b+=2){
+    console.log('juft=' , juft, 'b=' , b);
+        juft*=b
+}
+console.log(juft);
