7d73c029d98a9cb9bf5bf5270b5cb690254dc7b7
@NodirMN <998946140194@mail.ru>
2021-08-16 11:03:28 -1200

    Update JS-7.js

--- JS-7.js
+++ JS-7.js
@@ -1,9 +1,10 @@
 ///O'rin almashtirib o'sish bo'yicha tartiblash
+
 let mas = [55,44,2,3,43,51,22,37,83]
 console.log(mas);
 for(let i=0; i<mas.length;i++){
         for(let j=0;j<mas.length;j++){
-            if(mas[i]>mas[j]){
+            if(mas[i]<mas[j]){   /// > qisa pasayish tartibi bilan
                 let res = mas[i]
                 mas[i] = mas[j]
                 mas[j] = res
