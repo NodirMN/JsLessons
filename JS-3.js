e9bdf0fbaeefd631f2667405e6bf7aff751aeaa7
@NodirMN <998946140194@mail.ru>
2021-08-15 12:46:03 -1200

    Update JS-3.js

--- JS-3.js
+++ JS-3.js
@@ -7,9 +7,24 @@ for(let a = 0; a<=10; a++){
 console.log(summa);
 
 ///// Juft son
-let juft = 1
+let son = 1
 for(let b = 2; b<=10; b+=2){
-    console.log('juft=' , juft, 'b=' , b);
-        juft*=b
+    console.log('on=' , son, 'b=' , b);
+        son*=b
 }
-console.log(juft);
+console.log(son);
+
+//////Tekaris sonlar
+for (let n = 10; n >0; n--) { /////10.9.8....
+    console.log(n);
+}
+
+//////
+let m = 13
+let l = 3
+for (let  h=l;h<m;h++){
+    if (h==7){
+        break
+    }
+    console.log(h);
+}
