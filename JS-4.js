38c35e90a7d202cbdef82072462562c9fbcb8d46
@NodirMN <998946140194@mail.ru>
2021-08-15 13:44:56 -1200

    node JS

--- JS-4.js
+++ JS-4.js
@@ -5,3 +5,24 @@ while (w<=q) {
     w++
     console.log(w);
 }
+
+////////////break
+let e = 12
+let r = 0 // sochik
+while (r<=e) {
+    console.log(r);
+    if(r==22){
+        break
+    }
+    r++
+}
+
+///////////////juft kvadrati
+let y = 130
+let t = 1
+while(t<=r){
+    if(t%2==0){
+        console.log(t,  '    ', t*t);
+    }
+    t++
+}
