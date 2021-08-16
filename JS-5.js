e7b2b4b716c19a3090adcd6d85d7dac2ccb37f99
@NodirMN <998946140194@mail.ru>
2021-08-15 14:50:25 -1200

    v

---
+++ JS-5.js
@@ -0,0 +1,24 @@
+//////////Massiv uzunligi
+let humans = ['Nodir','Begzod','Sherzod']
+
+console.log(humans.length); //massiv uzunligi length
+for(let index = 0; index<humans.length; index++){
+        console.log(humans[index]);
+}
+
+///////////massiv uzumnligi length
+
+let numb = [22,33,32,12,5,6,78,]
+for (let qs=0; qs<numb.length; qs++){
+    console.log(numb[qs]);
+}
+
+/////////massiv qo'shish
+let mat = [22, 33, 32, 12, 5, 6, 78, ]
+let son = 0
+for (let lson=0; lson<mat.length; lson++){
+    son+=mat[lson]
+    console.log(mat[lson],son);
+}
+
+//////////////////////
